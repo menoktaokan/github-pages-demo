0ed83605d475b5932be5d25db257ad9afa06194a
m. okan <mokan.yilmaz@sikayetvar.dev>
2025-09-02 00:29:46 +0300

    message change

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 setTimeout(() => {
   const p = document.createElement("p");
-  p.innerText = "and it's dynamic, too!";
+  p.innerText = "ve bu da dinamik bir metin...";
   document.body.appendChild(p);
 }, 2000);
